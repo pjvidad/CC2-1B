fd667326c2f20677077566f95d90d3527d77d2e3
Princess Joy Vidad <145944544+pjvidad@users.noreply.github.com>
2023-09-27 08:47:33 +0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+age_input = int(input("What is your age? "))
+#assigning variables
+age = int(age_input)
+age_next_year = age + 1
+pi = 3.141592653589
+
+#print function
+print("Hello,", name)
+print(f"Your age next year is {age_next_year}")
+print(f"The value of pi is {pi}")
+
+#end = "" , sep = ""
